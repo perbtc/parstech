83b90e6dc0d9ff11d2d9bd3d511b6ac27793c298
akradimo <perbtc@outlook.com>
2025-05-06 13:16:54 +0400

    3

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    // اینجا می‌توانی متدهای index, create, store و ... را اضافه کنی
+}
