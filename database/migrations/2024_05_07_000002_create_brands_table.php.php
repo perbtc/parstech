6ed8a15102670bb7ceacefff3038332235a1313f
akradimo <perbtc@outlook.com>
2025-05-07 17:10:18 +0400

    004

--- database/migrations/2024_05_07_000002_create_brands_table.php.php
+++ database/migrations/2024_05_07_000002_create_brands_table.php.php
@@ -7,14 +7,14 @@ use Illuminate\Support\Facades\Schema;
 class CreateBrandsTable extends Migration
 {
     public function up()
-{
-    Schema::create('brands', function (Blueprint $table) {
-        $table->id();
-        $table->string('name');
-        $table->string('image')->nullable();
-        $table->timestamps();
-    });
-}
+    {
+        Schema::create('brands', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
     public function down()
     {
         Schema::dropIfExists('brands');
