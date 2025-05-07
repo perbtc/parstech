ebc3f909d4c34377f262d31d578dc1ff6a42094c
akradimo <perbtc@outlook.com>
2025-05-07 18:30:23 +0400

    006

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Http\Controllers;
-
+use App\Models\Unit;
 use App\Models\Product;
 use App\Models\Category;
 use App\Models\Brand;
--- database/migrations/2025_05_07_141250_create_units_table.php
+++ database/migrations/2025_05_07_141250_create_units_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('units', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+            Schema::create('units', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->timestamps();
+    });
     }
 
     /**
